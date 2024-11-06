e468e9cbd1aee9ea60c22ab8c1cd23e9fa506086
vasilyevl <166270353+vasilyevl@users.noreply.github.com>
2024-11-05 20:43:48 -0800

    Added missing IOResult.cs file

---
+++ Net_08/Libraries/HWControlCommon/Common/BaseClasses/IOResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAQFramework.Common
+{
+    [Flags]
+    public enum IOResults
+    {
+        NA = 0,
+        Success = 1,
+        Error = 2,
+        Timeout = 4,
+        NotSupported = 8,
+        NotReady = 16,
+        NotAvailable = 32,
+        NotApplicable = 64,
+        Cancelled = 128,
+        Warning = 256,
+        NotImplemented = 512,
+        Ignored = 1024
+    }
+}
